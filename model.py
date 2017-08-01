ea72e6dfe61e213b112c22381f3df8dff2028040
Student <naimjm@gmail.com>
2017-08-01 11:23:25 +0300

    add style and html

--- model.py
+++ model.py
@@ -3,9 +3,24 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class YourModel(Base):
-    __tablename__  = 'yourmodel'
-    id             = Column(Integer, primary_key=True)
+class User(Base):
+    __tablename__  = 'user'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    user_name = Column(first_name + " " + last_name)
+    email = (String(100))
+    password = (String(50))
+
+class Post(Base):
+	__tablename__  = 'post'
+	id = Column(Integer, primary_key=True)
+	user_id = relationship('User')
+	title = Column(Text ?(50))
+	content = Column(String(1000))
+	video_url = Column(String(500))
+	image_url = Column(String(500))
+	category = Column(String(20))
     # ADD YOUR FIELD BELOW ID
 
 # IF YOU NEED TO CREATE OTHER TABLE 
